b597cfd53dc38e2f01da716005ed81511093e153
Zerozany <691139128@qq.com>
2026-01-25 15:03:17 +0800

    update:增加了qml文件加密选项

--- cmake/importConfig.cmake
+++ cmake/importConfig.cmake
@@ -6,6 +6,8 @@ target_compile_features(${PROJECT_NAME}
 set(QT_QML_GENERATE_QMLLS_INI ON)
 
 set_target_properties(${PROJECT_NAME} PROPERTIES
+    QT_QML_COMPILE_SOURCES ON # 强制所有 QML 文件都被编译为 C++
+    QT_QML_SOURCE_PROTECTION ON # 开启源码保护
     QT_QMLCACHEGEN_ARGUMENTS "--direct-calls" # "--direct-calls" "--verbose"
 )
 
--- cmake/importPack.cmake
+++ cmake/importPack.cmake
@@ -6,7 +6,7 @@ if(WIN32)
             COMMAND "$ENV{Qt6_BIN_DIR}/windeployqt.exe"
             $<$<CONFIG:Debug>:--debug>
             $<$<CONFIG:Release>:--release>
-            --qmldir "${CMAKE_SOURCE_DIR}/view"
+            --qmldir "${CMAKE_BINARY_DIR}/${PROJECT_NAME}/view"
             "$<TARGET_FILE:${PROJECT_NAME}>"
             COMMENT "Deploying Qt dependencies for ${PROJECT_NAME}"
         )
